c7d3f9da55c0b7313ba39004e43be89bb43037da
Sias Mey <siamey@gmail.com>
2023-03-31 10:54:49 +0200

    feat: add basic cli interactions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use std::process::ExitCode;
 
-fn main() -> ExitCode{
-    println!("Hello, world!");
+fn main() -> ExitCode {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        println!("Please provide a file path");
+        return ExitCode::FAILURE;
+    }
 
+    let file_path: &str = &args[1];
+
+    println!("File path: {}", file_path);
     ExitCode::SUCCESS
 }
